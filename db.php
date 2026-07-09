e7155bde1f56af3492fb078ee3eab55915f64d70
ravindrawan <132208871+ravindrawan@users.noreply.github.com>
2026-07-09 08:35:09 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
+$host = 'ssd-extr-db';
 $user = 'root';
-$pass = 'Ravi@2025';
+$pass = '';
 $db = 'social_services_monthly';
 
 $conn = @new mysqli($host, $user, $pass, $db);
